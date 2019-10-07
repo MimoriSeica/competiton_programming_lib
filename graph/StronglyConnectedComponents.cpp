caa7efcdadc11548ff2f09a92f8793e7d3e9b3b3
MimoriSeica <zerons_0612@yahoo.co.jp>
2019-10-07 09:37:11 +0900

    トポロジカルソート

--- graph/StronglyConnectedComponents.cpp
+++ graph/StronglyConnectedComponents.cpp
@@ -20,12 +20,14 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll,ll> P;
 
-struct SCC{
-	vector<vector<int> > G;
-	vector<vector<int> > rG;
+
+struct SCC {
+	vector<vector<int>> G;
+	vector<vector<int>> rG;
 	vector<int> vs;
 	vector<bool> used;
 	vector<int> cmp;
+	vector<int> sorted_num; // SCC 後のトポロジカルソートの順番が入る
 	int sz,newsz;
 	SCC(int n):G(n),rG(n),used(n,false),cmp(n){sz = n;}
 
@@ -51,8 +53,8 @@ struct SCC{
 		}
 	}
 
-	//ＳＣＣ実行
-	int reMake(){
+	//SCC実行
+	int solve(){
 		vs.clear();
 		REP(i,sz)if(!used[i])dfs(i);
 		int k = 0;
@@ -64,10 +66,70 @@ struct SCC{
 		return newsz = k;
 	}
 
+	//SCC した後の頂点を返す
 	int scc_node(int n){
 		return cmp[n];
+	}
+
+	// 同じ頂点か
+	bool isSameGroup(int a, int b){
+		return cmp[a] == cmp[b];
+	}
+
+	// SCC 後のグラフのトポロジカルシート
+	void topological_sort() {
+		sorted_num.resize(newsz);
+        vector<int> cou(newsz);
+        vector<vector<int>> new_v(newsz);
+
+        REP(i, sz){
+            new_v[scc_node(i)].PB(i);
+        }
+
+        queue<int> q;
+        REP(i, newsz){
+            REP(j, new_v[i].size()){
+                int node = new_v[i][j];
+                REP(k, rG[node].size()){
+                    int aite = rG[node][k];
+                    if(isSameGroup(node, aite))continue;
+                    cou[i]++;
+                }
+            }
+            if(cou[i] == 0){
+                q.push(i);
+            }
+        }
+
+        int num = 0;
+        while(!q.empty()){
+            int now = q.front();q.pop();
+            sorted_num[now] = num;
+            num++;
+
+            REP(i, new_v[now].size()){
+                int node = new_v[now][i];
+                REP(j, G[node].size()){
+                    int aite = G[node][j];
+                    if(isSameGroup(node, aite))continue;
+                    cou[scc_node(aite)]--;
+
+                    if(cou[scc_node(aite)] == 0){
+                        q.push(scc_node(aite));
+                    }
+                }
+            }
+        }
+	}
+
+	// SCC 後のトポロジカル順序を返す
+	int topological_order(int node){
+		return sorted_num[cmp[node]];
 	}
 };
 
 int main(){
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
+
+	return 0;
 }
