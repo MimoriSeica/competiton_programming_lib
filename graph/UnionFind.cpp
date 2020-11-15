8154c71b29c6341ca4abceee2b7fbd2a6fa287a7
MimoriSeica <zerons_0612@yahoo.co.jp>
2020-11-16 01:22:32 +0900

    簡略化

--- graph/UnionFind.cpp
+++ graph/UnionFind.cpp
@@ -1,12 +1,10 @@
-struct UF
-{
+struct UF {
 	vector<int> par; // 親のインデックスを記憶する配列
 	vector<int> sz; // サイズを記憶する。
-	vector<int> rank;
 	// 初期化
-	UF(int n):par(n),sz(n),rank(n){
+	UF(int n):par(n),sz(n){
 		for(int i = 0; i < n; i++){
-			par[i] = i;sz[i] = 1;rank[i] = 0;
+			par[i] = i;sz[i] = 1;
 		}
 	}
 	// 親を求める
@@ -18,15 +16,10 @@ struct UF
 	void unite(int x, int y) {
 		x = find(x); y = find(y);
 		if (x == y) return;
-		if(rank[x] < rank[y]){
-			par[x] = y;
-			sz[y] += sz[x];
-		}
-		else{
-			par[y] = x;
-			sz[x] += sz[y];
-			if(rank[x] == rank[y])rank[x]++;
-		}
+		if(sz[x] > sz[y])swap(x, y);
+		par[x] = y;
+		sz[y] += sz[x];
+		// マージデクはここでやる
 	}
 	// xとyが同じ集合ならtrue
 	bool same(int x, int y) { return find(x) == find(y); }
