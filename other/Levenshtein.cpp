2f2b6d7b6cffae07087675a25c7109bbff21ce1e
MimoriSeica <zerons_0612@yahoo.co.jp>
2021-09-09 01:41:12 +0900

    スタイル変更

--- other/Levenshtein.cpp
+++ other/Levenshtein.cpp
@@ -4,19 +4,18 @@
 using namespace std;
 #define D 10000
 int dp[D][D];
-void LS(string a,string b)
-{
-	REP(i,a.size()+1)dp[i][0]=i;
-	REP(i,b.size()+1)dp[0][i]=i;	
-	for(int i=1;i<=a.size();i++)
-	{
-		for(int j=1;j<=b.size();j++)
-		{
-			int q=1;if(a[i-1]==b[j-1])q = 0;
-			dp[i][j]=min(min(dp[i-1][j-1]+q,dp[i][j-1]+1),dp[i-1][j]+1);
+void LS(string a,string b) {
+	REP(i,a.size()+1) dp[i][0] = i;
+	REP(i,b.size()+1) dp[0][i] = i;
+	for(int i = 1;i <= a.size();i++) {
+		for(int j = 1; j<= b.size();j++) {
+			int q = 1;
+			if(a[i-1] == b[j-1])q = 0;
+			dp[i][j] = min(min(dp[i-1][j-1]+q, dp[i][j-1]+1), dp[i-1][j]+1);
 		}
-	}	
+	}
 }
+
 int main()
 {
 	string a,b;cin>>a>>b;
